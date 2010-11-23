a18db96e7cd442068f7348ce08f84f004939fa06
xbwang <vincent.wangxb@gmail.com>
2010-11-22 20:02:37 -0600

    Bounce v0.1

---
+++ Java Homework4/Problem2/Bounce.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Bounce{
+	public static void main(String[] args){
+		JFrame bFrame = new BounceFrame();
+		frame.show();
+	}
+}
+
+class BounceFrame extends JFrame{
+	private JPanel canvas;
+	
+	public BounceFrame(){
+		
+	}
+}
