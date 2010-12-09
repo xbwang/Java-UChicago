6e64e588a25f53165d26c26d57e7998a3be578fa
xbwang <vincent.wangxb@gmail.com>
2010-12-08 20:03:12 -0600

    Problem1 done

--- Java Homework4/Problem1/LoginForm.java
+++ Java Homework4/Problem1/LoginForm.java
@@ -36,14 +36,42 @@ class LoginForm extends JFrame{
 
 	exitBut = new JButton("exit");
 	cp.add(exitBut);
-
+	
 	//add event handlers here
-	MyActionListener al = new MyActionListener(this); //I write this class
-	submitBut.addActionListener(al);
-	clearBut.addActionListener(al);
-	exitBut.addActionListener(al);
+	submitBut.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent evt){
+			String login = tf.getText();
+		    char[] password = pf.getPassword();
+		    String passwordAsString = new String(password);
+			boolean isValidLogin = false;
 
-	setSize(200,150);
+			if(passwordAsString.equals("CSPP") && login.equals("CSPP")){
+				isValidLogin = true;
+			}
+			if (isValidLogin){
+				JOptionPane.showMessageDialog(null,"You may log in");
+		    }
+		    else{
+				JOptionPane.showMessageDialog(null,"Invalid User!");
+		    }
+		}
+	});
+	
+	clearBut.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent evt){
+			tf.setText(null);
+		    pf.setText(null);
+		    return;
+		}
+	});
+	
+	exitBut.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent evt){
+			System.exit(1);
+		}
+	});
+	
+	setSize(200,200);
 	show();
 
     }
@@ -52,40 +80,4 @@ class LoginForm extends JFrame{
 	new LoginForm();
     }
 
-}
-
-
-class MyActionListener implements ActionListener{
-    private LoginForm form;
-
-    MyActionListener(LoginForm form){
-	this.form = form;
-    }
-
-    public void actionPerformed(ActionEvent ae){
-	String whichButton = ae.getActionCommand();
-
-	if (whichButton.equals("clear")){
-	    this.form.tf.setText(null);
-	    this.form.pf.setText(null);
-	    return;
-	}
-	if (whichButton.equals("exit")){
-	    System.exit(1);
-	}
-	
-	if (whichButton.equals("submit")){
-	    String login = this.form.tf.getText();
-	    char[] password = this.form.pf.getPassword();
-	    String passwordAsString = new String(password);
-	    boolean isValidLogin = LoginUtilities.checkLogin(login,passwordAsString);
-	    if (isValidLogin){
-		JOptionPane.showMessageDialog(null,"You may log in");
-	    }
-	    else{
-		JOptionPane.showMessageDialog(null,"Invalid User!");
-	    }
-	}
-	    
-    }
 }
